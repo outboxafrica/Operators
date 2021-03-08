ea3f587f1aea21bdb7e56aa0785c552191cc3df7
Scovia Achan <achanscovia@gmail.com>
2021-03-09 02:26:58 +0300

    create cards for upcoming events

--- src/Pages/GeneralHomePage/GeneralHomePage.jsx
+++ src/Pages/GeneralHomePage/GeneralHomePage.jsx
@@ -35,32 +35,39 @@ function GeneralHomePage() {
 
             <div>
                 <div className="hero-section">
-                    <h1>Welcome to EDU</h1>
+                    <h1>Welcome to OutBox EDU</h1>
                     <span>The learning platform during this period</span>
                     <div className="buttons">
-                        <Button>SignUp</Button>
-                        <Button>Log in</Button>
+                            <Button style={{margin:"10px", width:"100px"}} variant="contained" color="primary"><Link to="/register">Register</Link></Button>
+                                              
+                            <Button style={{margin:"10px", width:"100px"}} variant="contained" color="primary"><Link to="sighIn">Sign in</Link></Button>
+                        
                     </div>
                 </div>
                 
             </div>
 
-            <div className="all-events">
-                <div className="event">
-                    <h3>Student's forum</h3>
-                    <img id="new-img"/>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, repellendus quasi. Quisquam, saepe ex consequatur 
-                        blanditiis est incidunt eius animi hic dignissimos possimus debitis doloribus vitae recusandae. Modi, molestiae commodi!</p>
+            <h3 style={{textAlign: "center", color:"rgb(56, 122, 221)"}}>Upcoming events</h3>
 
-                        <Button>Read More</Button>
+            <div className="all-events">
+                <div className="std-event">
+                    
+                    <img className="event-img" src="https://images.unsplash.com/photo-1530099486328-e021101a494a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTh8fHN0dWRlbnR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"/>
+                    <h3>Dev Night</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, repellendus quasi. Quisquam, saepe ex consequatur 
+                        blanditiis est incidunt eius animi hic dignissimos possimus debitis doloribus vitae recusandae. Modi, molestiae commodi Modi, molestiae commodi!</p>
+                    
+                    <Button variant="outlined" color="primary">Read More</Button>
 
                 </div>
-                <div className="event">
+                <div className="std-event">
+                    
+                    <img className="event-img" src="https://images.unsplash.com/photo-1599687351724-dfa3c4ff81b1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8c3R1ZGVudHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"/>
                     <h3>Get Together</h3>
-                    <img />
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, repellendus quasi. Quisquam, saepe ex consequatur 
                         blanditiis est incidunt eius animi hic dignissimos possimus debitis doloribus vitae recusandae. Modi, molestiae commodi!</p>
-                    <Button>Read More</Button>
+                   
+                    <Button variant="outlined" color="primary">Read More</Button>
                     
 
                 </div>
