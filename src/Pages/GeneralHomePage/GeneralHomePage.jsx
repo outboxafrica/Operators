cf1c2543349367953be2fde67453600f8b5aff87
Scovia Achan <achanscovia@gmail.com>
2021-03-10 10:01:06 +0300

    modify home-page cards

--- src/Pages/GeneralHomePage/GeneralHomePage.jsx
+++ src/Pages/GeneralHomePage/GeneralHomePage.jsx
@@ -40,65 +40,57 @@ function GeneralHomePage() {
           <h1>Welcome to OutBox EDU</h1>
           <span>The learning platform during this period</span>
           <div className="buttons">
-            {user.person}
-            {user.person === "null" || typeof user.person == "undefined" ? (
-              <Link to="/sighIn">
-                <Button
-                  style={{ margin: "10px", width: "90px", fontSize:"0.9rem" }}
-                  variant="contained"
-                  color="primary"
-                  className=""
-                >
-                  Login
-                </Button>
-              </Link>
-            ) : (
-              <Link to="/profileHome">
-                <Button>Home</Button>
-              </Link>
-            )}
+                {user.person}
+                {user.person === "null" || typeof user.person == "undefined" ? (
+                <Link to="/sighIn">
+                    <Button
+                    style={{ margin: "10px"}}
+                    variant="outlined"
+                    color="primary"
+                    className=""
+                    >
+                    Login
+                    </Button>
+                </Link>
+                ) : (
+                <Link to="/profileHome">
+                    <Button>Home</Button>
+                </Link>
+                )}
 
-            {user.person === "null" || typeof user.person == "undefined" ? (
-              <Link to="/register">
-                <Button
-                  style={{ margin: "10px" }}
-                  variant="contained"
-                  color="primary"
-                >
-                  Sign up
-                </Button>
-              </Link>
-            ) : (
-              <Link to="/profileHome">
-                <Button
-                  style={{ margin: "10px" }}
-                  variant="contained"
-                  color="primary"
-                >
-                  Sign up
-                </Button>
-              </Link>
-            )}
-            {/* <Button
-              style={{ margin: "10px", width: "100px" }}
-              variant="contained"
-              color="primary"
-            >
-              <Link to="/register">Register</Link>
-            </Button>
-
-            <Button
-              style={{ margin: "10px", width: "100px" }}
-              variant="contained"
-              color="primary"
-            >
-              <Link to="/sighIn">Sign in</Link>
-            </Button> */}
+                {user.person === "null" || typeof user.person == "undefined" ? (
+                <Link to="/register">
+                    <Button
+                    style={{ margin: "10px" }}
+                    variant="outlined"
+                    color="primary"
+                    >
+                    Sign up
+                    </Button>
+                </Link>
+                ) : (
+                <Link to="/profileHome">
+                    <Button
+                    style={{ margin: "10px" }}
+                    variant="contained"
+                    color="primary"
+                    >
+                    Sign up
+                    </Button>
+                </Link>
+                )}
           </div>
         </div>
       </div>
 
-      <h3 style={{ textAlign: "center", color: "rgb(56, 122, 221)", fontSize: "2.5rem" }}>
+      <h3
+        style={{
+          textAlign: "center",
+          color: "rgb(56, 122, 221)",
+          fontSize: "1.8rem",
+          padding: "15px"
+        }}
+      >
         Upcoming events
       </h3>
 
@@ -108,7 +100,23 @@ function GeneralHomePage() {
             className="event-img"
             src="https://images.unsplash.com/photo-1530099486328-e021101a494a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTh8fHN0dWRlbnR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
           />
-          <h3>Dev Night</h3>
+          <h3>Coaching Session</h3>
+          <p>
+            Join hundreds of students for a live coaching session and get the best careere advice to kickstart your tech journey. The program
+            has trained so far 1200 students and all these have moved a step further to their goals, with some landing opportunities of a lifetime
+          </p>
+
+          <Button variant="outlined" color="primary">
+            Read More
+          </Button>
+        </div>
+
+        <div className="std-event">
+          <img
+            className="event-img"
+            src="https://images.unsplash.com/photo-1530099486328-e021101a494a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTh8fHN0dWRlbnR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+          />
+          <h3>The </h3>
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam,
             repellendus quasi. Quisquam, saepe ex consequatur blanditiis est
@@ -120,17 +128,15 @@ function GeneralHomePage() {
             Read More
           </Button>
         </div>
+
         <div className="std-event">
           <img
             className="event-img"
             src="https://images.unsplash.com/photo-1599687351724-dfa3c4ff81b1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8c3R1ZGVudHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
           />
-          <h3>Get Together</h3>
+          <h3>One on One Mentorship Session</h3>
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam,
-            repellendus quasi. Quisquam, saepe ex consequatur blanditiis est
-            incidunt eius animi hic dignissimos possimus debitis doloribus vitae
-            recusandae. Modi, molestiae commodi!
+            We pair students with mentors to guide them in their career journey. These mentors provide advice (career and general life) to help you achieve your goals 
           </p>
 
           <Button variant="outlined" color="primary">
